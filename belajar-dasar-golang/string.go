7d8fb3c0899ee9bf4171d47bf6bca355562bb4f4
Addin Hadi Rizal <addinhadi10403@gmail.com>
2025-06-22 10:58:07 +0700

    update

---
+++ belajar-dasar-golang/string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Addin")
+	fmt.Println("Addin Hadi")
+	fmt.Println("Addin Hadi Rizal")
+
+	fmt.Println(len("Addin"))
+	fmt.Println("Addin Hadi" [2]) // ini yg diambil itu byte nya atau karakternya bukan tipe data stringnya
+	fmt.Println("Addin Hadi Rizal")
+	
+}
---
+++ belajar-dasar-golang/variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string
+
+	name  = "Addin Hadi Rizal"
+	fmt.Println(name)
+
+	name = "Addin Hadi"
+	fmt.Println(name)
+
+	var name2 = "Addin"
+	fmt.Println(name2)
+	
+
+}
